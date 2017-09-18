a56738825c5abceccfed1b625f7e8bbae19b7bde
nastyalebedzeva <31952626+nastyalebedzeva@users.noreply.github.com>
2017-09-18 20:50:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var begBrackets = [];
+	var endBrackets = [];
+	var indx;
+	var result;
+  for (var i = 0; i < str.length; i++) {
+// search for opening brackets
+	  indx = bracketsConfig.indexOf(str[i]);
+	  if(indx == 0){
+		  if(i == str.length-1) {
+			  result = false;
+			  break;
+		  }
+		  begBrackets.push[indx];
+	  }
+	  if(indx !== 0){
+		  if(i == 0){
+			  result = false;
+			  break;
+		  }
+		  endBrackets.push[indx];
+	  }
+  }
+  
+  if(result == false){
+	  return false;
+  } else {
+	  if(begBrackets.length == endBrackets.length){
+		  return true;
+	  }else{
+		  return false;
+	  }
+  } 
 }
